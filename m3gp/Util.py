75d26da5585d8a265ce42ec316491322e044a7c6
jebatista <jebatista@fc.ul.pt>
2020-09-28 12:54:15 +0100

    Now using inv() from numpy.linalg to calculate inverse matrices

--- m3gp/Util.py
+++ m3gp/Util.py
@@ -38,46 +38,7 @@ def inverseMatrix(m):
 	Returns the inverse of the matrix m, if possible,
 	otherwise returns the diagonal matrix
 	'''
-	m = deepcopy(m)
-	n = len(m)
-	inv = np.eye(n)
-	d = 0
-
-	for i in range(n):
-		if m[i][i]==0:
-			for k in range(i+1,n):
-				if m[k][i] != 0:
-					tmp = m[i]
-					m[i] = m[k]
-					m[k] = tmp
-
-					tmp = inv[i]
-					inv[i] = inv[k]
-					inv[k] = tmp
-
-					break
-		if m[i][i] == 0:
-			return np.eye(n)
-
-		for y in range(i+1,n):
-			d = -m[y][i] / m[i][i]
-			for k in range(n):
-				inv[y][k] += inv[i][k]*d
-				m[y][k] += m[i][k]*d
-
-	if m[n-1][n-1] == 0:
-		return np.eye(n)
-
-	for i in range(n):
-		d = m[i][i]
-		for k in range(n):
-			inv[i][k] /= d 
-			m[i][k] /= d 
-
-	for x in range(n-1,0,-1):
-		for y in range(x-1,-1,-1):
-			d =  -m[y][x]
-			for k in range(n):
-				inv[y][k] += inv[x][k] * d 
-			m[y][x] += m[x][x] * d 
-	return inv
+	try:
+		return np.linalg.inv(np.array(m))
+	except:
+		return np.eye( len(m) )
