0d415e4165aacff0e59fbb402905893a61577cce
João Batista <jesp.batista@gmail.com>
2021-08-05 13:42:31 +0100

    Now supporting arithmetic operators with any number of arguments. Log2(x) and Max(x0,x1,..,xn) are implemented as examples.

--- Arguments.py
+++ Arguments.py
@@ -11,7 +11,9 @@ from sys import argv
 
 # Operators to be used by the models
 # Only these operators are available. To add mode, edit m3gp.Node.calculate(self, sample)
-OPERATORS = ["+","-","*","/"]
+
+#OPERATORS = [("+",2),("-",2),("*",2),("/",2),("log",1), ("max", 3)] # Example
+OPERATORS = [("+",2),("-",2),("*",2),("/",2)] # Default
 
 # Initial Maximum depth
 MAX_DEPTH = 6
--- m3gp/Node.py
+++ m3gp/Node.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+from math import log
+
+import warnings
+warnings.filterwarnings("ignore")
+
 # 
 # By using this file, you are agreeing to this product's EULA
 #
@@ -9,8 +14,7 @@ import numpy as np
 #
 
 class Node:
-	left = None
-	right = None
+	branches = None
 	value = None
 
 
@@ -20,46 +24,52 @@ class Node:
 
 	def create(self, rng, operators=None, terminals=None, depth=None,full=False):
 		if depth>1 and (rng.random()<0.5 or full ==True ):
-			self.value = operators[rng.randint(0,len(operators)-1)]
-			self.left = Node()
-			self.left.create(rng, operators, terminals, depth-1)
-			self.right = Node()
-			self.right.create(rng, operators, terminals, depth-1)
+			op, n_args = operators[rng.randint(0,len(operators)-1)]
+			self.value = op
+
+			self.branches = []
+			for i in range(n_args):
+				n = Node()
+				n.create(rng, operators, terminals, depth-1)
+				self.branches.append(n)
 		else:
 			self.value = terminals[rng.randint(0,len(terminals)-1)] # Sem literais
 
 
-	def copy(self, left=None,value=None,right=None):
-		self.left = left
-		self.right=right
+	def copy(self,value=None, branches=None):
+		self.branches = branches
 		self.value=value
 
 
 	def __str__(self):
-		if self.left == None:
+		if self.branches == None:
 			return str(self.value)
 		else:
-			return "( " + str(self.left) + " " + str(self.value) + " " + str(self.right) + " )"
+			if len(self.branches) == 2:
+				return "( " + str(self.branches[0]) + " " + str(self.value) + " " + str(self.branches[1]) + " )"
+			else:
+				return str(self.value) + " ( " + " ".join( [str(b) for b in self.branches] ) + " )"
 
 
 	def getSize(self):
 		'''
 		Returns the total number of nodes within this Node.
 		'''
-		if self.left == None:
+		if self.branches == None:
 			return 1
 		else:
-			return self.left.getSize() + 1 + self.right.getSize()
+
+			return 1 + sum( [b.getSize() for b in self.branches] )
 
 
 	def getDepth(self):
 		'''
 		Returns the depth of this Node.
 		'''
-		if self.left == None:
+		if self.branches == None:
 			return 1
 		else:
-			return 1 + max(self.left.getDepth(),self.right.getDepth())
+			return 1 + max( [b.getDepth() for b in self.branches] )
 
 
 	def getRandomNode(self, rng, value=None):
@@ -69,41 +79,42 @@ class Node:
 		if value == None:
 			value = rng.randint(0,self.getSize()-1)
 		if value == 0:
+			#print(self)
 			return self
 
-		left_size = self.left.getSize()
-		if value <= left_size :
-			return self.left.getRandomNode(rng, value-1)
-		else:
-			return self.right.getRandomNode(rng, value-left_size-1)
+		#print(value, self)
+		for i in range(len(self.branches)):
+			size = self.branches[i].getSize()
+			if value-1 < size:
+				return self.branches[i].getRandomNode(rng, value-1)
+			value -= size
 
 
 	def swap(self, other):
 		'''
 		Swaps the content of two nodes.
 		'''
-		l = self.left
+		b = self.branches
 		v = self.value
-		r = self.right
-		self.left = other.left
+
+		self.branches = other.branches
 		self.value = other.value
-		self.right = other.right
-		other.left = l
+
+		other.branches = b
 		other.value = v
-		other.right = r
 
 
 	def clone(self):
 		'''
 		Returns a clone of this node.
 		'''
-		if self.left == None:
+		if self.branches == None:
 			n = Node()
-			n.copy(left = None, value=self.value, right = None)
+			n.copy(value=self.value, branches = None)
 			return n
 		else:
 			n = Node()
-			n.copy(left = self.left.clone(), value=self.value, right=self.right.clone())
+			n.copy(value=self.value, branches=[b.clone() for b in self.branches])
 			return n
 
 
@@ -112,7 +123,7 @@ class Node:
 		'''
 		Returns the calculated value of a sample.
 		'''
-		if self.left == None:
+		if self.branches == None:
 			try:
 				return np.array( sample[self.value] )#.astype("float64")
 			except:
@@ -121,22 +132,32 @@ class Node:
 				
 		else:
 			if self.value == "+": #+
-				return self.left.calculate(sample) + self.right.calculate(sample)
+				return self.branches[0].calculate(sample) + self.branches[1].calculate(sample)
 			if self.value == "-": #-
-				return self.left.calculate(sample) - self.right.calculate(sample)
+				return self.branches[0].calculate(sample) - self.branches[1].calculate(sample)
 			if self.value == "*": #*
-				return self.left.calculate(sample) * self.right.calculate(sample)
+				return self.branches[0].calculate(sample) * self.branches[1].calculate(sample)
 			if self.value == "/": #/
-				right = self.right.calculate(sample)
+				right = self.branches[1].calculate(sample)
 				right = np.where(right==0, 1, right)
-				return self.left.calculate(sample) / right
-
+				return self.branches[0].calculate(sample) / right
+			if self.value == "log2": # log2(X)
+				res = self.branches[0].calculate(sample)
+				res = np.where(res<=0, res, np.log2(res))
+				return res
+			if self.value == "max": # max( X0, X1, ... Xn)
+				calc = [b.calculate(sample) for b in self.branches]
+				a = []
+				for i in range(len(calc[0])):
+					a.append( max([calc[k][i] for k in range(len(calc))]) )
+				return np.array(a)
+				
 
 	def isLeaf(self):
 		'''
 		Returns True if the Node had no sub-nodes.
 		'''
-		return self.left == None
+		return self.branches == None
 
 	def getSemantics(self,tr_x):
 		'''
@@ -149,8 +170,7 @@ class Node:
 		Assigns the content of another Node to this Node.
 		'''
 		self.value = other.value
-		self.left = other.left
-		self.right = other.right
+		self.branches = other.branches
 
 	def prun(self, tr_x):
 		'''
@@ -160,35 +180,34 @@ class Node:
 		semantics.sort()
 		if semantics[0]== semantics[-1] and len(semantics)>1:
 			self.value = str(semantics[0])
-			self.left = None
-			self.right = None
+			self.branches = None
 		# [+, -, *, /]
 		
 		# +
 		if self.value == "+":
 			# 0 + X == X
-			if not self.isLeaf() and ( self.left.isLeaf() and self.left.value == "0.0" ):
-				self.redirect(self.right)
+			if not self.isLeaf() and ( self.branches[0].isLeaf() and self.branches[0].value == "0.0" ):
+				self.redirect(self.branches[1])
 
 			# X + 0 == X
-			if not self.isLeaf() and ( self.right.isLeaf() and self.right.value == "0.0" ):
-				self.redirect(self.left)
+			if not self.isLeaf() and ( self.branches[1].isLeaf() and self.branches[1].value == "0.0" ):
+				self.redirect(self.branches[0])
 
 			# X + X == 2 * X
-			if not self.isLeaf() and ( str(self.right) == str(self.left) ):
+			if not self.isLeaf() and ( str(self.branches[1]) == str(self.branches[0]) ):
 				self.value = "*"
 				n = Node()
 				n.copy(value = "2.0")
-				self.left.redirect( n )
+				self.branches[0].redirect( n )
 
 		# - 
 		if self.value == "-":
 			# X - 0 == X
-			if not self.isLeaf() and ( self.right.isLeaf() and self.right.value == "0.0" ):
-				self.redirect(self.left)
+			if not self.isLeaf() and ( self.branches[1].isLeaf() and self.branches[1].value == "0.0" ):
+				self.redirect(self.branches[0])
 
 			# X - X == 0
-			if not self.isLeaf() and ( str(self.right) == str(self.left) ):
+			if not self.isLeaf() and ( str(self.branches[1]) == str(self.branches[0]) ):
 				n = Node()
 				n.copy(value = "0.0")
 				self.redirect( n )
@@ -196,39 +215,37 @@ class Node:
 		# * 
 		if self.value == "*":
 			# X * 0 == 0,  0 * X == 0
-			if not self.isLeaf() and ( (self.left.isLeaf() and self.left.value=="0.0") or (self.right.isLeaf() and self.right.value=="0.0") ):
+			if not self.isLeaf() and ( (self.branches[0].isLeaf() and self.branches[0].value=="0.0") or (self.branches[1].isLeaf() and self.branches[1].value=="0.0") ):
 				n = Node()
 				n.copy(value = "0.0")
 				self.redirect( n )
 
 			# 1 * X == X
-			if not self.isLeaf() and ( self.left.isLeaf() and self.left.value == "1.0" ):
-				self.redirect(self.right)
+			if not self.isLeaf() and ( self.branches[0].isLeaf() and self.branches[0].value == "1.0" ):
+				self.redirect(self.branches[1])
 
 			# X * 1 == X
-			if not self.isLeaf() and ( self.right.isLeaf() and self.right.value == "1.0" ):
-				self.redirect(self.left)
+			if not self.isLeaf() and ( self.branches[1].isLeaf() and self.branches[1].value == "1.0" ):
+				self.redirect(self.branches[0])
 
 		# //
 		if self.value == "/":
 			# X // 0 == 1
-			if not self.isLeaf() and ( self.right.isLeaf() and self.right.value=="0.0" ):
+			if not self.isLeaf() and ( self.branches[1].isLeaf() and self.branches[1].value=="0.0" ):
 				n = Node()
 				n.copy(value = "1.0")
 				self.redirect( n )
 
 			# X // 1 == X
-			if not self.isLeaf() and ( self.right.isLeaf() and self.right.value=="1.0" ):
-				self.redirect(self.left)
+			if not self.isLeaf() and ( self.branches[1].isLeaf() and self.branches[1].value=="1.0" ):
+				self.redirect(self.branches[0])
 
 			# X // X == 1
-			if not self.isLeaf() and ( str(self.right) == str(self.left) ):
+			if not self.isLeaf() and ( str(self.branches[1]) == str(self.branches[0]) ):
 				n = Node()
 				n.copy(value = "1.0")
 				self.redirect( n )
 
-		if self.left != None:
-			self.left.prun(tr_x)
-
-		if self.right != None:
-			self.right.prun(tr_x)	
+		if self.branches != None:
+			for branch in self.branches:
+				branch.prun(tr_x)
